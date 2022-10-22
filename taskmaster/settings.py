c1a8c5a18c8df0043feaae2a43fe560b7f9a89eb
mzxz <mohamed.abaker110@gmail.com>
2022-10-22 21:11:40 +0000

    Add env.py file and import it to settings.py

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s0g-w2ltbg9&%e=g3%up0(=3_@ekj)_z3d7x##$fd%qr$)xd3u'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
