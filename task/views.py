9c628b11fd68cd53d158fb485b7edb8ad76e4bb1
mzxz <mohamed.abaker110@gmail.com>
2022-11-17 19:42:25 +0000

    Added base template, contact and about html pages

--- task/views.py
+++ task/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def todolist(request):
-    return render(request, 'todolist.html', {})
+    context = {'welcome_text':"Welcome from Jinja2."}
+
+    return render(request, 'todolist.html', context)
