d096e3e5e5328e6b8ae8461f05020f4d940f65a1
mzxz <mohamed.abaker110@gmail.com>
2022-11-17 19:56:11 +0000

    Linked contact & about pages to urls and views

--- task/urls.py
+++ task/urls.py
@@ -3,5 +3,7 @@ from task import views
 
 urlpatterns = [
     path('', views.todolist, name='todolist'),
+    path('contact', views.contact, name='contact'),
+    path('about-us', views.about, name='about'),
 
 ]
--- task/views.py
+++ task/views.py
@@ -4,6 +4,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def todolist(request):
-    context = {'welcome_text':"Welcome from Jinja2."}
-
+    context = {
+        'welcome_text':"Welcome From Todo List Page."
+        }
     return render(request, 'todolist.html', context)
+
+def contact(request):
+    context = {
+        'welcome_text':"Welcome From Contact Page."
+        }
+    return render(request, 'contact.html', context)
+
+def about(request):
+    context = {
+        'welcome_text':"Welcome From About Us Page."
+        }
+    return render(request, 'about.html', context)
