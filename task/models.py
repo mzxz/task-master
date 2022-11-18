5f5f731daf33b9029ad47a907ddfea21c403f504
mzxz <mohamed.abaker110@gmail.com>
2022-11-18 23:17:41 +0000

    Updated DATABASE_URL to ElephantSQL

--- task/models.py
+++ task/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class TaskList(models.model):
+
+
+class TaskList(models.Model):
     task = models.CharField(max_length=300)
     done = models.BooleanField(default=False)
