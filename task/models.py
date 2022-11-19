b4fa7242063ec2d5cbb297db4c8113a706989ace
mzxz <mohamed.abaker110@gmail.com>
2022-11-19 15:33:00 +0000

    register first model

--- task/models.py
+++ task/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class TaskList(models.Model):
     task = models.CharField(max_length=300)
     done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.task + " - " + str(self.done)
