f2e75a9e8a244bad9f1eec6b775d76fc683c7377
mzxz <mohamed.abaker110@gmail.com>
2022-11-19 22:28:45 +0000

    Fetch data from Database & display as a tabele on todolist section

--- task/views.py
+++ task/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from task.models import TaskList
 
 
 # Create your views here.
 def todolist(request):
-    context = {
-        'welcome_text':"Welcome From Todo List Page."
-        }
-    return render(request, 'todolist.html', context)
+    all_tasks = TaskList.objects.all
+   
+    return render(request, 'todolist.html', {'all_tasks': all_tasks})
 
 
 def contact(request):
