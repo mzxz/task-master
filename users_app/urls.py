a31ef95782691b73bab84c78a2221ab1403858b4
mzxz <mohamed.abaker110@gmail.com>
2022-12-08 14:36:40 +0000

    Add login page, urls, views, & template

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from users_app import views
+from django.contrib.auth import views as auth_viewa
 
 urlpatterns = [
     path('register', views.register, name='register'),
+    path('login', auth_viewa.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', auth_viewa.LogoutView.as_view(), name='logout'),
+
 ]
