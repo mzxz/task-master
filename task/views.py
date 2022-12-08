98f0e81ea436cadfd7d3304eeb6033c1ab5bd836
mzxz <mohamed.abaker110@gmail.com>
2022-12-08 17:36:59 +0000

    Used decorator to add restrictions to pages that can only accessed by the user

--- task/views.py
+++ task/views.py
@@ -4,10 +4,11 @@ from django.core.paginator import Paginator
 from django.contrib import messages
 from task.models import TaskList
 from task.forms import TaskForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
+@login_required()
 def todolist(request):
     if request.method == "POST":
         form = TaskForm(request.POST or None)
@@ -23,12 +24,14 @@ def todolist(request):
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
 
 
+@login_required()
 def delete_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.delete() 
     return redirect('todolist')
 
 
+@login_required()
 def edit_task(request, task_id):
     if request.method == "POST":
         task = TaskList.objects.get(pk=task_id)
@@ -43,6 +46,7 @@ def edit_task(request, task_id):
         return render(request, 'edit.html', {'task_obj': task_obj})
 
 
+@login_required()
 def complete_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.done = True    
@@ -51,6 +55,7 @@ def complete_task(request, task_id):
     return redirect('todolist')
 
 
+@login_required()
 def pending_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.done = False    
--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -149,3 +149,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = "todolist"
+LOGIN_URL = "login"
