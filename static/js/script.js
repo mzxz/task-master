537a452b53f77244c435632e7405172827dfa935
mzxz <mohamed.abaker110@gmail.com>
2022-12-23 19:02:15 +0000

    add footer, styles & font font awesome

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,4 @@
-const submitNewTask = document.getElementById("submit-new-task");
-const addNewTask = document.getElementById("add-new-task");
-
-addNewTask.addEventListener("keyup", (e) => {
-    const value = e.currentTarget.value;
-    submitNewTask.disabled = false;
-
-    if (value == " "){
-        submitNewTask.disabled = true;
-    }
-});
-
+function validate(input){
+    if(/^\s/.test(input.value))
+        input.value = '';
+}
